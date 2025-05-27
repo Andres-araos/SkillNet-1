a6f01ceb24d052a1df2354d26f728c50b6cd339c
Andres Araos <andresemilioaraos@gmail.com>
2025-05-26 23:59:39 -0500

    css

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="LeonardElMasMakia123@ñ",        
+        password="",        
         database="skillnet"
     )
